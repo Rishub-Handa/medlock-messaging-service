4fc941b5578cf8414da09e53a2d3fe21fddc6f8b
Rishub-Handa <rh4gh@virginia.edu>
2020-10-19 12:10:04 -0400

    modified testing file

--- test/apiTest.py
+++ test/apiTest.py
@@ -2,7 +2,7 @@ import requests
 
 
 url = "http://localhost:5000/api/registerPt" 
-data = {"name": "Rishub Handa", "phoneNum": "+17326667043", "reminderTimes": "11:53", "followUpTime": "11:54", "finalReminderTime": "11:55", "emergencyContact": "Mom (phone number)"} 
+data = {"name": "Rishub Handa", "phoneNum": "+17326667043", "reminderTimes": "10:00", "followUpTime": "18:00", "finalReminderTime": "20:00", "emergencyContact": "Mom (phone number)"} 
 requests.post(url, data) 
 
 url = "http://localhost:5000/api/beginStudy"
